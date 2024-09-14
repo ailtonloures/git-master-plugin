6cf2572eb22e2575a37450fcc8e4b6c56f21ece7
Ailton Loures <dev.ailtonloures@proton.me>
2024-09-14 15:01:46 -0300

    feat: adding alert messages on init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+source $(dirname $0)/message.sh
+
 if [ -t 0 ]; then
     main_path=$(echo "$(pwd)/main.sh")
 
     chmod +x $main_path
     git config --global alias.master "!bash $main_path"
+
+    if [ $? -ne 0 ]; then
+        show_danger_msg "Error: An error occurred while trying to configure Git Master."
+        exit 1
+    fi
+
+    show_success_msg "Git master has been successfully configured!"
+    exit 0
 fi
