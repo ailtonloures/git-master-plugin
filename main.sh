b909d8be1ec19ace239f44c5e3a002ac2adb855f
Ailton Loures <dev.ailtonloures@proton.me>
2024-10-14 22:42:00 -0300

    feat: add validation to main, master and release branches

--- main.sh
+++ main.sh
@@ -5,35 +5,46 @@ source "$(dirname "$0")"/message.sh
 source "$(dirname "$0")"/functions.sh
 
 if [ -t 0 ]; then
-	current_branch=$(git branch --show-current) # get the current branch
+	# get the current branch
+	current_branch=$(git branch --show-current)
 
 	echo -e "\n!Initializing the Git Master on branch $current_branch!\n"
 
-	if [ ! -d ".git" ]; then # checks if repository has been initialized
+	# checks if repository has been initialized
+	if [ ! -d ".git" ]; then
 		show_danger_msg "Error: The git repository not configured properly."
 		show_info_msg "Please use git init to initialize a new git repository..."
-		exit 1 # exit with generic error
+		# exit with generic error
+		exit 1
 	fi
 
-	git_remote=$(git remote) # get the remote from the git repository
+	# get the remote from the git repository
+	git_remote=$(git remote)
 
-	if [ -z "$git_remote" ]; then # check if remote exists
+	# check if remote exists
+	if [ -z "$git_remote" ]; then
 		show_danger_msg "Error: Remote repository address is not configured."
 		show_info_msg "Please use git remote add origin <newurl> to set the remote."
-		exit 1 # exit with generic error code
+		# exit with generic error code
+		exit 1
 	fi
 
 	fetch_branches "$git_remote"
 
-	git_branch=$(git branch --all --list '*feature*' --list '*hotfix*' --list '*fix*' --list '*release*' | grep -v "^\*") # list all branches (feature/hotfix/fix/release)
+	# list all branches (feature/hotfix/fix/release)
+	git_branch=$(git branch --all --list '*feature*' --list '*hotfix*' --list '*fix*' --list '*release*' | grep -v "^\*")
 
-	if [ -z "$git_branch" ]; then # check if branch list is empty
+	# check if branch list is empty
+	if [ -z "$git_branch" ]; then
 		show_warning_msg "There are no branches available to merge."
-		exit 1 # exit with generic error code
+		# exit with generic error code
+		exit 1
 	fi
 
-	branches_list=()        # create list of options
-	merged_branches_list=() # merged branches list will start empty
+	# create list of options
+	branches_list=()
+	# merged branches list will start empty
+	merged_branches_list=()
 
 	for branch in "${git_branch[@]}"; do
 		renamed_branch=$(echo "$branch" | sed -e "s/remotes\///g")
@@ -43,41 +54,51 @@ if [ -t 0 ]; then
 	opt_list=($(printf "%s\n" "${branches_list[@]}" | sort | uniq) "Exit")
 
 	echo -e "-> Listing all branches...\n"
-	PS3="Choose a branch to merge: " # select input
+	# select input
+	PS3="Choose a branch to merge: "
 
-	select opt in "${opt_list[@]}"; do # list all selectable branches
+	# list all selectable branches
+	select opt in "${opt_list[@]}"; do
 		case $opt in
 		"Exit")
 			# Exit option
 			show_success_msg "Exit..."
-			exit 0 # exit with success code
+			# exit with success code
+			exit 0
 			;;
 		*)
 			# Default option
-			if [ -z "$opt" ]; then # check if an option does not exist
+			# check if an option does not exist
+			if [ -z "$opt" ]; then
 				show_danger_msg "Invalid option!"
 			else
 				target_branch=$opt
 
 				merge_branch "$target_branch"
-				merge_error_code=$? # store the error code from git fetch update
+				# store the error code from git fetch update
+				merge_error_code=$?
 
 				if [ $merge_error_code -ne 0 ]; then
-					check_merge_conflicts 0 # checks for merge conflicts recursively
+					# checks for merge conflicts recursively
+					check_merge_conflicts 0
 
-					if [ $? -eq 1 ]; then # checks that conflicts have not been resolved correctly
+					# checks that conflicts have not been resolved correctly
+					if [ $? -eq 1 ]; then
 						show_danger_msg "Error: Failed to merge branch $target_branch. With error code $merge_error_code"
-						exit $merge_error_code # exit with git merge error code
+						# exit with git merge error code
+						exit $merge_error_code
 					fi
 				fi
 
 				show_success_msg "Merged branch $target_branch successfully!"
 
-				merged_branches_list+=("$target_branch") # add the target branch to merged branches list
+				# add the target branch to merged branches list
+				merged_branches_list+=("$target_branch")
 
 				question "Do you want to continue?"
 
-				if [ $? -eq 1 ]; then # check if user doesn't want to continue the merge operation
+				# check if user doesn't want to continue the merge operation
+				if [ $? -eq 1 ]; then
 					echo -e "-> Finishing the merge step..."
 					break
 				fi
@@ -86,45 +107,54 @@ if [ -t 0 ]; then
 		esac
 	done
 
-	if [ ${#merged_branches_list[@]} -eq 0 ]; then # check if has merged branches on list
+	# check if has merged branches on list
+	if [ ${#merged_branches_list[@]} -eq 0 ]; then
 		show_warning_msg "No branch has been merged."
-		exit 1 # exit with generic error code
+		# exit with generic error code
+		exit 1
 	fi
 
 	echo -e "-> Merged branches:\n"
 
-	for i in "${!merged_branches_list[@]}"; do # get the index from the merged branch list
-		branch=${merged_branches_list[$i]}        # get the branch name from index
-		index=$((i + 1))                          # get the index and increment
+	# get the index from the merged branch list
+	for i in "${!merged_branches_list[@]}"; do
+		# get the branch name from index
+		branch=${merged_branches_list[$i]}
+		# get the index and increment
+		index=$((i + 1))
 
 		echo -e "\t$index - $branch\r"
 	done
 
-	echo -e "\n"
-
 	question "Do you want to push the current branch ($current_branch)?"
 	question_error_code=$?
 
-	if [ $question_error_code -eq 0 ]; then # check if user wants to push the current branch
+	# check if user wants to push the current branch
+	if [ $question_error_code -eq 0 ]; then
 		push_branch "$git_remote" "$current_branch"
 	fi
 
-	question "Do you want to create a new tag?"
-	question_error_code=$?
+	if [ "$current_branch" == "main" ] || [ "$current_branch" == "master" ] || [[ "$current_branch" == *"release"* ]]; then
+		question "Do you want to create a new tag?"
+		question_error_code=$?
 
-	if [ $question_error_code -eq 0 ]; then # check if user wants to create a new tag
-		echo "-> Tag name: "
-		read -r value # get the tag name
-		create_and_push_tags "$git_remote" "$value"
-	fi
+		# check if user wants to create a new tag
+		if [ $question_error_code -eq 0 ]; then
+			# get the tag name
+			read -r -p "-> Tag name: " value
+			create_and_push_tags "$git_remote" "$value"
+		fi
 
-	question "Do you want to delete the merged branches?"
-	question_error_code=$?
+		question "Do you want to delete the merged branches?"
+		question_error_code=$?
 
-	if [ $question_error_code -eq 0 ]; then         # check if user wants to delete the merged branches
-		for branch in "${merged_branches_list[@]}"; do # get the branch name from the merged branch list
-			delete_branch "$git_remote" "$branch"
-		done
+		# check if user wants to delete the merged branches
+		if [ $question_error_code -eq 0 ]; then
+			# get the branch name from the merged branch list
+			for branch in "${merged_branches_list[@]}"; do
+				delete_branch "$git_remote" "$branch"
+			done
+		fi
 	fi
 
 	show_success_msg "Git Master finished successfully!"
